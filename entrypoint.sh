82ae039ec306b71bcd31d8530985f1fdbd71a39a
Anthony Brown <anthony.brown8@nhs.net>
2026-02-16 17:40:54 +0000

    correct syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-echo "cd work" > /tmp/input_run.sh
+echo "cd /work" > /tmp/input_run.sh
 echo ${INPUT_RUN} >> /tmp/input_run.sh
 chmod +x /tmp/input_run.sh
 
-echo "contents of /tmp/input_run.sh"
-cat /tmp/input_run.sh
-echo "Going to run docker run"
-
-exec docker run 
+exec docker run \
     -v "/var/run/docker.sock":"/var/run/docker.sock" \
     -v /tmp/input_run.sh:/tmp/input_run.sh \
     -v ${INPUT_WORKSPACE_FOLDER}:/work \

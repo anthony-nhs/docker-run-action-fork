4d62f8d0e1ae07c4b0abad4eac02db8fa7bc984b
Anthony Brown <anthony.brown8@nhs.net>
2026-02-16 15:01:59 +0000

    write stuff to a script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
-fi
+echo "cd work" > /tmp/input_run.sh
+echo ${INPUT_RUN} >> /tmp/input_run.sh
+chmod +x /tmp/input_run.sh
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
-fi
-
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run 
+    -v "/var/run/docker.sock":"/var/run/docker.sock" \
+    -v /tmp/input_run.sh:/tmp/input_run.sh \
+    -v ${WORKSPACE_FOLDER}:/work \
+    -u 1001:1001 \
+    --workdir /work \
+    $INPUT_OPTIONS \
+    $INPUT_IMAGE \
+    -c /tmp/input_run.sh

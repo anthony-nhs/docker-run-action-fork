5600f5880dac8d150a7dc10820f5b01922b0b08e
Anthony Brown <anthony.brown8@nhs.net>
2026-02-16 18:13:29 +0000

    correct docker run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-echo "cd /work" > /tmp/input_run.sh
-echo ${INPUT_RUN} >> /tmp/input_run.sh
+echo ${INPUT_RUN} > /tmp/input_run.sh
 chmod +x /tmp/input_run.sh
 
 exec docker run \
+    -i \
+    --rm \
     -v "/var/run/docker.sock":"/var/run/docker.sock" \
-    -v /tmp/input_run.sh:/tmp/input_run.sh \
     -v ${INPUT_WORKSPACE_FOLDER}:/work \
     -u 1001:1001 \
     --workdir /work \
     $INPUT_OPTIONS \
     $INPUT_IMAGE \
-    sh < /tmp/input_run.sh
+    $INPUT_SHELL < /tmp/input_run.sh

df2f1ddaa0d7f80e0b95404d81a1ee0919e37985
Anthony Brown <anthony.brown8@nhs.net>
2026-02-16 17:37:47 +0000

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ echo "cd work" > /tmp/input_run.sh
 echo ${INPUT_RUN} >> /tmp/input_run.sh
 chmod +x /tmp/input_run.sh
 
+echo "contents of /tmp/input_run.sh"
+cat /tmp/input_run.sh
+echo "Going to run docker run"
+
 exec docker run 
     -v "/var/run/docker.sock":"/var/run/docker.sock" \
     -v /tmp/input_run.sh:/tmp/input_run.sh \
